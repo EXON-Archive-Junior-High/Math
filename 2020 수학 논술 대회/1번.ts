383b76e5e83ab4ac8f8665db9eb57a0b45911f5e
시혁 박 <exon.indiv@gmail.com>
2020-10-21 22:35:54 +0900

    ===

--- 2020 수학 논술 대회/1번.ts
+++ 2020 수학 논술 대회/1번.ts
@@ -4,7 +4,7 @@ for (let A: number = 0; A < 10; A++) {
             for (let D: number = 0; D < 10; D++) {
                 for (let E: number = 0; E < 10; E++) {
                     let num: number = (A * 10000) + (B * 1000) + (C * 100) + (D * 10) + E
-                    if (num * 4 == (E * 10000) + (D * 1000) + (C * 100) + (B * 10) + A && num !== 0) console.log(num)
+                    if (num * 4 === (E * 10000) + (D * 1000) + (C * 100) + (B * 10) + A && num !== 0) console.log(num)
                 }
             }
         }
